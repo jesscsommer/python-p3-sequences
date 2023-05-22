4e86609e21cf72f2a1eb1d7479f278e74a112a45
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-22 09:21:49 -0700

    Complete lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    my_list = []
+    if length == 1: 
+        my_list.append(0)
+    elif length == 2: 
+        my_list.append(0)
+        my_list.append(1)
+    elif length > 2: 
+        my_list.append(0)
+        my_list.append(1)
+        for i in range(2, length): 
+            my_list.append(my_list[i - 1] + my_list[i - 2])
+    else: 
+        pass
+    print(my_list)
